0a55e20fb8ef5a7f2fbb06c5ffbb6d0afb129d4f
elnish <a.shobotova@hotmail.com>
2019-09-26 08:45:32 +0300

    add clean css

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 var gulp = require('gulp'),
-scss = require('gulp-sass'),
-autoprefixer = require('gulp-autoprefixer');
+    scss = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    clean = require('gulp-clean-css');
 
 gulp.task('scss', () => {
 return gulp.src('src/scss/style.scss')
     .pipe(scss())
     .pipe(autoprefixer())
+    .pipe(clean())
     .pipe(gulp.dest('src/css'))
    
 });
 
+
 gulp.task('watch', () => {
-gulp.watch('src/scss/*.scss', gulp.parallel('scss'));
+    gulp.watch('src/scss/*.scss', gulp.parallel('scss'));
 });
 
 gulp.task('default', gulp.parallel('scss', 'watch'));
